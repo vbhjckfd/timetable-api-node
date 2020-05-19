f0fe6b1236d8178c108ae879c30cc26c8333b231
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-05-19 19:08:24 +0300

    Bus-route direction bind

--- actions/routeDynamicInfoAction.js
+++ actions/routeDynamicInfoAction.js
@@ -16,24 +16,49 @@ const dbConfig = {
 module.exports = async (req, res, next) => {
     mongoose.connect(process.env.MONGO_GTFS_URL, dbConfig);
 
-    const routes = await gtfs.getRoutes();
-
-    let route = _(routes).find((i) => {
-        return i.route_short_name === normalizeRouteName(req.params.name);
-    });
+    const route = (await gtfs.getRoutes({
+        route_short_name: normalizeRouteName(req.params.name)
+    })).shift();
 
     const response = await fetch('http://track.ua-gis.com/gtfs/lviv/vehicle_position');
     const body = await response.buffer();
 
+    let trips = await gtfs.getTrips({
+        'route_id': route.route_id
+    });
+
+    let tripShapeMap = {};
+    let shapeIdsStat = [];
+    trips.forEach((t) => {
+        tripShapeMap[t.trip_id] = t.shape_id;
+        shapeIdsStat.push(t.shape_id);
+    });
+
+    let mostPopularShapes = _(shapeIdsStat)
+        .countBy()
+        .entries()
+        .orderBy(_.last)
+        .takeRight(2)
+        .map(_.head)
+        .value();
+
+    let goodTripIds = _(trips)
+        .filter((t) => {return mostPopularShapes.includes(t.shape_id)})
+        .map((t) => {return t.trip_id})
+        .uniq()
+        .value()
+    ;
+
     let vehicles = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body).entity
     .filter((entity) => {
-        return entity.vehicle.trip.routeId == route.route_id && !!entity.vehicle.trip.tripId
+        return entity.vehicle.trip.routeId == route.route_id && !!entity.vehicle.trip.tripId && goodTripIds.includes(entity.vehicle.trip.tripId)
     })
     .map((i) => {
         let position = i.vehicle.position;
 
         return {
             'id': i.vehicle.vehicle.id,
+            'direction': mostPopularShapes.indexOf(tripShapeMap[i.vehicle.trip.tripId]),
             'location': [
                 position.latitude,
                 position.longitude
