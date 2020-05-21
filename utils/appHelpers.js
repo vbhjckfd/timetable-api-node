6f1df3febbc20ecdc6499d6fe92e4c7bae8db000
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-05-21 12:10:59 +0300

    Move from Easyway to Microgiz

--- utils/appHelpers.js
+++ utils/appHelpers.js
@@ -26,5 +26,24 @@ module.exports = {
         }
 
         return prefix + ((rawNumber > 10) ? rawNumber : ('0' + rawNumber));
-    }    
+    },
+
+    getRouteType: (route) => {
+        let type = 'bus';
+        if (route.route_short_name.startsWith('Тр')) {
+            type = 'trol';
+        } else if (route.route_short_name.startsWith('Т')) {
+            type = 'tram';
+        }
+        return type;
+    },
+
+    cleanUpStopName: (stopName) => {
+        return stopName.replace(/(\([\-\d]+\))/gi, '').trim();
+    },
+
+    getTextWaitTime: (busArrivalTime) => {
+        let minutesLeft = Math.round((busArrivalTime - new Date()) / 1000 / 60);
+        return ((minutesLeft > 0) ? minutesLeft : '< 1') + 'хв';
+    }
 }
