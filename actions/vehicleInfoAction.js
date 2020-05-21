c0ef5a40f255f9db9e0905b0c51be977c90e2301
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-05-21 19:08:01 +0300

    Added next stop arrival times

--- actions/vehicleInfoAction.js
+++ actions/vehicleInfoAction.js
@@ -3,6 +3,8 @@ const _ = require('lodash');
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const fetch = require("node-fetch");
 const appHelpers = require("../utils/appHelpers");
+const timetableDb = require('../connections/timetableDb');
+const StopModel = timetableDb.model('Stop');
 
 module.exports = async (req, res, next) => {
     const response = await fetch('http://track.ua-gis.com/gtfs/lviv/vehicle_position');
@@ -35,15 +37,43 @@ module.exports = async (req, res, next) => {
         .map(_.head)
         .value();
 
+    const tripUpdatesResponse = await fetch('http://track.ua-gis.com/gtfs/lviv/trip_updates');
+    const tripUpdatesBody = await tripUpdatesResponse.buffer();
+
+    const arrivalTimeItems = _(GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(tripUpdatesBody).entity)
+    .find((entity) => {
+        return entity.tripUpdate.vehicle.id == req.params.vehicleId;
+    }) || null;
+
+    let arrivalTimes = arrivalTimeItems ? arrivalTimeItems.tripUpdate.stopTimeUpdate : []
+
+    const stopIds = arrivalTimes.map((i) => {return i.stopId});
+
+    const stopIdsMap = _(await StopModel.find({
+        microgiz_id: {
+            $in: stopIds
+        }
+    }))
+    .keyBy('microgiz_id')
+    .value();
+
+    arrivalTimes = arrivalTimes.filter((item) => {return !!stopIdsMap[item.stopId]})
+
     res
         .set('Cache-Control', `public, s-maxage=15`)
         .send({
-            'location': [
+            location: [
                 vehiclePosition.position.latitude,
                 vehiclePosition.position.longitude
             ],
-            'bearing': vehiclePosition.position.bearing,
-            'direction': mostPopularShapes.indexOf(tripShapeMap[vehiclePosition.trip.tripId]),
-            'licensePlate': vehiclePosition.vehicle.licensePlate
+            bearing: vehiclePosition.position.bearing,
+            direction: mostPopularShapes.indexOf(tripShapeMap[vehiclePosition.trip.tripId]),
+            licensePlate: vehiclePosition.vehicle.licensePlate,
+            arrivals: arrivalTimes.map((item) => {
+                return {
+                    code: stopIdsMap[item.stopId].code,
+                    arrival: (new Date(parseInt(`${item.arrival.time.low}000`))).toUTCString()
+                };
+            })
         });
 }
