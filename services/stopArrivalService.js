e42cb379f47503c18292f48bf7fddb72a38f6a0b
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-06-18 15:58:58 +0300

    Try to fallback if bad db was imported

--- services/stopArrivalService.js
+++ services/stopArrivalService.js
@@ -68,8 +68,8 @@ const stopArrivalService = {
                 color: appHelpers.getRouteColor(routes[vh.route_id]),
                 route: appHelpers.formatRouteName(routes[vh.route_id]),
                 vehicle_type: appHelpers.getRouteType(routes[vh.route_id]),
-                lowfloor: !!trips[vh.trip_id].wheelchair_accessible,
-                end_stop: appHelpers.cleanUpStopName(trips[vh.trip_id].trip_headsign),
+                lowfloor: trips[vh.trip_id] ? !!trips[vh.trip_id].wheelchair_accessible : false,
+                end_stop: trips[vh.trip_id] ? appHelpers.cleanUpStopName(trips[vh.trip_id].trip_headsign) : '',
                 arrival_time: (new Date(vh.time)).toUTCString(),
                 time_left: appHelpers.getTextWaitTime(vh.time),
                 ...vehiclesLocations[vh.vehicle]
