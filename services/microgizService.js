70b17306d4798a95390a4f1e1ad78e7e050cbca9
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-07-20 19:10:07 +0300

    Add retry to GTFS realtime calls

--- services/microgizService.js
+++ services/microgizService.js
@@ -4,15 +4,7 @@ const gtfs = require('gtfs');
 const _ = require('lodash');
 const appHelpers = require("../utils/appHelpers");
 
-const timetableDb = require('../connections/timetableDb');
-const StopModel = timetableDb.model('Stop');
-
 let Promise = require('bluebird');
-//const redisClient = Promise.promisifyAll(require("./redisClient"));
-
-const ARRIVALS_CACHE_KEY = 'arrival-times';
-const LOCATION_CACHE_KEY = 'vehicles-locations';
-const STOP_ROUTE_MAP_KEY = 'stop-route-map-hash';
 
 module.exports = {
 
@@ -25,42 +17,26 @@ module.exports = {
     },
 
     getVehiclesLocations: () => {
-        // return redisClient.getAsync(LOCATION_CACHE_KEY)
-        //     .then((data) => {
-        //         if (data) {
-        //             return JSON.parse(data);
-        //         }
-
-                return fetch(process.env.VEHICLES_LOCATION_URL || 'http://track.ua-gis.com/gtfs/lviv/vehicle_position')
-                    .then(response => response.buffer())
-                    .then(data => {
-                        const parsedData = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data).entity;
-                        //redisClient.setex(LOCATION_CACHE_KEY, 10, JSON.stringify(parsedData));
-
-                        return parsedData;
-                    });
-        // });
+        return fetch(process.env.VEHICLES_LOCATION_URL || 'http://track.ua-gis.com/gtfs/lviv/vehicle_position')
+            .then(response => response.buffer())
+            .then(data => GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data).entity)
+            .catch(err => {
+                console.error(err);
+                return module.exports.getVehiclesLocations();
+            });
     },
 
     getArrivalTimes: () => {
-        // return redisClient.getAsync(ARRIVALS_CACHE_KEY)
-        //     .then((data) => {
-        //         if (data) {
-        //             return JSON.parse(data);
-        //         }
-
-                return fetch(process.env.TRIP_UDPDATES_URL || 'http://track.ua-gis.com/gtfs/lviv/trip_updates')
-                    .then(response => response.buffer())
-                    .then(data => {
-                        const parsedData = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data).entity;
-                        //redisClient.setex(ARRIVALS_CACHE_KEY, 10, JSON.stringify(parsedData));
-
-                        return parsedData;
-                    });
-        // });
+        return fetch(process.env.TRIP_UDPDATES_URL || 'http://track.ua-gis.com/gtfs/lviv/trip_updates')
+            .then(response => response.buffer())
+            .then(data => GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data).entity)
+            .catch(err => {
+                console.error(err);
+                return module.exports.getArrivalTimes();
+            });
     },
 
-routesThroughStop: async (stop) => {
+    routesThroughStop: async (stop) => {
         let routes = new Map();
 
         const stopTimes = await gtfs.getStoptimes({
