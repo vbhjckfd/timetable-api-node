9a30cb0d7d04a7e68fec86c81d73724a9df2e065
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-07-20 12:55:32 +0300

    Refactor closest stops search

--- actions/getClosestStopsAction.js
+++ actions/getClosestStopsAction.js
@@ -3,13 +3,13 @@ const StopModel = timetableDb.model('Stop');
 const appHelpers = require("../utils/appHelpers");
 
 module.exports = async (req, res, next) => {
-    let latitude = parseFloat(req.query.latitude).toFixed(3);
-    let longitude = parseFloat(req.query.longitude).toFixed(3);
+    let latitude = parseFloat(req.query.latitude).toFixed(2);
+    let longitude = parseFloat(req.query.longitude).toFixed(2);
 
     StopModel.find({
         location: {
             $near: {
-                $maxDistance: 600, //meters
+                $maxDistance: 1000, //meters
                 $geometry: {
                     type: "Point",
                     coordinates: [longitude, latitude]
@@ -17,7 +17,11 @@ module.exports = async (req, res, next) => {
             }
         }
     }).find((error, results) => {
-        if (error) throw error;
+        if (error) {
+            console.error(error);
+
+            return res.status(500).send(`Bad argument: ${JSON.stringify([latitude, longitude])}`);
+        }
 
         let cacheLine = `public, max-age=0, s-maxage=${appHelpers.secondsUntilImportDone()}, stale-while-revalidate=15`;
         if (!results.length) {
