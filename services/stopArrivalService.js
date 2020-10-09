9bd3c155f1f8be77fadb3bc9925d472f4ed41d46
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-10-09 18:56:33 +0300

    Add direction to timetable

--- services/stopArrivalService.js
+++ services/stopArrivalService.js
@@ -80,6 +80,7 @@ const stopArrivalService = {
             }
 
             return {
+                direction: trips[vh.trip_id] ? trips[vh.trip_id].direction_id : null,
                 lowfloor: trips[vh.trip_id] ? !!trips[vh.trip_id].wheelchair_accessible : false,
                 end_stop: trips[vh.trip_id] ? appHelpers.cleanUpStopName(trips[vh.trip_id].trip_headsign) : '',
                 arrival_time: (new Date(vh.time)).toUTCString(),
