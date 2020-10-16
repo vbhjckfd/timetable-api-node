51875c9ceeed9cdd6ca25d19523240e81a18574b
Myroslav Holyak <myroslav.holyak@leisure-group.eu>
2020-10-16 11:21:04 +0300

    Revert fix

--- utils/appHelpers.js
+++ utils/appHelpers.js
@@ -142,14 +142,6 @@ module.exports = {
             }
         }, {shape_id: 1, _id: 0});
 
-        Object.defineProperty(Array.prototype, 'flat', {
-            value: function(depth = 1) {
-              return this.reduce(function (flat, toFlatten) {
-                return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
-              }, []);
-            }
-        });
-
         const existingShapeIds = new Set(
             Array.from(existingShapeRaw)
             .flat()
