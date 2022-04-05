b6d42c6928ee00136ef54334f968a20fbdf7370b
Myroslav Holyak <myroslav.holyak@olx.com>
2022-04-05 15:05:31 +0300

    More info on all stops page

--- actions/getAllRoutesAction.js
+++ actions/getAllRoutesAction.js
@@ -1,4 +1,5 @@
 const timetableDb = require('../connections/timetableSqliteDb');
+const _ = require('lodash');
 
 module.exports = async (req, res, next) => {
     await timetableDb.loadDatabase();
@@ -10,11 +11,46 @@ module.exports = async (req, res, next) => {
         .data()
     ;
 
-    let result = '<table>';
+
+
+    let result = `
+        <style>
+        table {border-collapse: collapse;}
+        table, th{
+            text-align: left;
+        }
+        table tr {border-bottom: 1pt solid black;}
+        table td {vertical-align: top;}
+        a {
+            text-decoration: none;
+        }
+        </style>
+        <table>
+    `;
     for (let r of routesRaw) {
+
+        const allStops = _(timetableDb.getCollection('stops').find({
+            code: {
+                $in: Object.values(r.stops_by_shape).flat()
+            }
+        })).keyBy('code').value();
+
+        stopsByShape = []
+        for (key of [0, 1]) {
+            stopsByShape[key] = _(r.stops_by_shape[String(key)])
+                .filter(st => !!allStops[st])
+                .map(st => allStops[st])
+                .map(s => ({code: s.code, name: s.name}))
+                .map(s => `<li>${s.name} (<a target="_blank" href="https://lad.lviv.ua/${s.code}">${s.code}</a>)</li>`)
+                .value()
+                .join('');
+        }
+
       result += `<tr>
-        <td><a target="blank" href="https://lad.lviv.ua/route/${r.external_id}">${r.short_name}</a></td>
+        <td><a target="_blank" href="https://lad.lviv.ua/route/${r.external_id}">${r.short_name}</a></td>
         <td>${r.long_name}</td>
+        <td><ol>${stopsByShape[0]}</ol></td>
+        <td><ol>${stopsByShape[1]}</ol></td>
         </tr>`;
     }
     result += '</table>';
